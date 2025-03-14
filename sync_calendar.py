6179f5e58e85d69dd009f5944b9177dd21a25450
Nuno Teixeira <61828740+nunojct12@users.noreply.github.com>
2025-03-14 18:58:57 +0000

    Update first version

--- sync_calendar.py
+++ sync_calendar.py
@@ -1,8 +1,28 @@
-import manage_events
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
+from gcsa.recurrence import Recurrence, DAILY, SU, SA
+
+from beautiful_date import Jan, Apr
 
 
-def main():
-  manage_events.manage_events()
+calendar = GoogleCalendar(
+    "nunojct12@gmail.com", credentials_path=".credentials/credentials.json"
+)
+event = Event(
+    "Test",
+    start=(1 / Apr / 2025)[9:00],
+    # recurrence=[
+    #     Recurrence.rule(freq=DAILY),
+    #     Recurrence.exclude_rule(by_week_day=[SU, SA]),
+    #     Recurrence.exclude_times([
+    #         (19 / Apr / 2019)[9:00],
+    #         (22 / Apr / 2019)[9:00]
+    #     ])
+    # ],
+    minutes_before_email_reminder=50,
+)
 
-if __name__ == "__main__":
-  main()
+calendar.add_event(event)
+
+for event in calendar:
+    print(event)
